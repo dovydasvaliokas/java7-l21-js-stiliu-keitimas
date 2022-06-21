f66d67f570c29a1dbde9310e68dc8585ea29e047
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-21 12:37:19 +0300

    Baigtas pirmas pavyzdys, kur pakeicia fono spalva ir teksto spalva

--- pvz1-spalvu-keitimas/script.js
+++ pvz1-spalvu-keitimas/script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-    
+    let pastraipa = document.getElementById("pastraipa");
+    let antraste = document.getElementById("antraste");
+    let mygtukas = document.getElementById("mygtukas");
+
+    mygtukas.onclick = function() {
+        antraste.style.backgroundColor = "rgb(25, 15, 100)";
+        pastraipa.style.color = "blue";
+    }
 }
