d6bb63bd426579fb6f8ea5afd813efee69b3a58f
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-21 13:00:55 +0300

    baigtas hide toggle pavyzdys

--- pvz2-elemento-toggle-paslėpimas/script.js
+++ pvz2-elemento-toggle-paslėpimas/script.js
@@ -1,3 +1,20 @@
 window.onload = function () {
-    
+    let mygtukas = document.getElementById("mygtukas");
+    let pastraipa = document.getElementById("pastraipa");
+
+    mygtukas.onclick = function() {
+        togglePaslepti(pastraipa);
+    }
+}
+
+
+function togglePaslepti(elementas) {
+    if (elementas.style.display != "none") {
+        console.log("adsasdasd");
+        elementas.style.display = "none";
+    }
+    else {
+        console.log("thyutreutyiuty");
+        elementas.style.display = "initial";
+    }
 }
